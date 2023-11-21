f343f8753fa189dea25609d877c2589366718e80
Exist2023 <leon.szameitat2007@gmail.com>
2023-11-21 21:50:35 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up routes for different pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/grade', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'grade.html'));
+});
+
+app.get('/class', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'class.html'));
+});
+
+// Set up a connection event
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle disconnection events
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  // Rest of code...
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
